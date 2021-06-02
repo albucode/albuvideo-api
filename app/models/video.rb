27f1312d6b0a36b71a979e02f61318799d48687e
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-02 13:32:33 -0700

    Validate public_id presence, uniqueness and length

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,5 +7,9 @@ class Video < ApplicationRecord
 
   enum status: { processing: 0, ready: 1, failed: 2 }
 
-  validates :published, :source, :status, presence: true
+  validates :published, :source, :status, :public_id, presence: true
+
+  validates :public_id, uniqueness: true
+
+  validates :public_id, length: { is: 10 }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe Video, type: :model do
   end
 
   it { is_expected.to validate_presence_of(:published) }
+
   it { is_expected.to validate_presence_of(:source) }
+
   it { is_expected.to validate_presence_of(:status) }
+
+  it { is_expected.to validate_presence_of(:public_id) }
+
+  it { is_expected.to validate_uniqueness_of(:public_id) }
+
+  it { is_expected.to validate_length_of(:public_id).is_equal_to(10) }
 end
