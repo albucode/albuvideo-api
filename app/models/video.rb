f1b3d3706e5d441cdebc794c57ba56a95fe61760
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-02 11:36:14 -0700

    Set public_id values to random string

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Video < ApplicationRecord
+  after_initialize do |video|
+    video.public_id = SecureRandom.alphanumeric(10)
+  end
+
   enum status: { processing: 0, ready: 1, failed: 2 }
 
   validates :published, :source, :status, presence: true
