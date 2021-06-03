c66449292eaaac544e336ca125f3f36ceb760ba7
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-03 15:01:28 -0700

    Extract find_video into private method

--- app/controllers/Api/videos_controller.rb
+++ app/controllers/Api/videos_controller.rb
@@ -1,20 +1,26 @@
 class Api::VideosController < ApplicationController
+  before_action :find_video, except: [:index]
+
   def index
     videos = Video.all
     render json: videos
   end
 
   def show
-    video = Video.find_by(public_id: params[:id])
-    render json: video
+    render json: @video
   end
 
   def destroy
-    video = Video.find_by(public_id: params[:id])
-    if video.destroy
+    if @video.destroy
       render json: {}
     else
       render json: { message: 'This video could not be deleted' }
     end
+  end
+
+  private
+
+  def find_video
+    @video = Video.find_by(public_id: params[:id])
   end
 end
