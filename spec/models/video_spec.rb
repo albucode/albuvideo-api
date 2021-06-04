aa3470c78f41ed5f3a6bd4b74214c75c76132249
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-04 13:43:44 -0700

    Remove false positive test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -8,9 +8,7 @@ RSpec.describe Video, type: :model do
   it ' has a valid factory' do
     expect(video.validate!).to eq(true)
   end
-
-  it { is_expected.to validate_inclusion_of(:published).in_array([true, false]) }
-
+  
   it { is_expected.to validate_presence_of(:source) }
 
   it { is_expected.to validate_presence_of(:status) }
