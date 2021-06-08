87bd0787764551b65865923a2fa772309e92e48d
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-08 13:53:06 -0700

    Associate user and videos

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :recoverable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  has_many :videos
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,8 @@ class Video < ApplicationRecord
     self.public_id = SecureRandom.alphanumeric(10) if public_id.nil?
   end
 
+  belongs_to :user
+
   enum status: { processing: 0, ready: 1, failed: 2 }, _default: :processing
 
   validates :source, :status, :public_id, presence: true
