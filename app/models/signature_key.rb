ba932218e9973aad70b5049107d5127aab526fdb
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-14 16:03:46 -0700

    Validate SignatureKey

--- app/models/signature_key.rb
+++ app/models/signature_key.rb
@@ -2,4 +2,20 @@
 
 class SignatureKey < ApplicationRecord
   belongs_to :user
+
+  after_initialize do
+    self.public_id = SecureRandom.alphanumeric(10) if public_id.nil?
+  end
+
+  after_initialize do
+    self.signature_key = SecureRandom.alphanumeric(64) if signature_key.nil?
+  end
+
+  validates :signature_key, :public_id, :name, presence: true
+
+  validates :signature_key, :public_id, uniqueness: true
+
+  validates :signature_key, length: { is: 64 }
+
+  validates :public_id, length: { is: 10 }
 end
