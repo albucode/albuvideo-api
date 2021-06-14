b1a75cbcd3c09f1750c50e9b1ead2665f4f9489a
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-14 12:56:45 -0700

    Ch43/create accesstoken model and endpoints (#9)
    * Create AccessToken model
    
    * Create endpoints for AccessToken
    
    * Test access_token controller
    
    * Do not use instance variables
    
    * Rename controller spec file
    
    * Fix spec synthax
    
    * Make name presence mandatory

---
+++ db/migrate/20210609230920_create_access_tokens.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateAccessTokens < ActiveRecord::Migration[6.1]
+  def change
+    create_table :access_tokens do |t|
+      t.string :name, null: false
+      t.string :access_token, limit: 32, null: false
+      t.string :public_id, limit: 10, null: false
+      t.references :user, null: false, foreign_key: true
+      t.index :access_token, unique: true
+      t.index :public_id, unique: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/access_token_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AccessToken, type: :model do
+  subject(:access_token) { FactoryBot.build(:access_token) }
+
+  it ' has a valid factory' do
+    expect(access_token.validate!).to eq(true)
+  end
+
+  it { is_expected.to validate_presence_of(:access_token) }
+
+  it { is_expected.to validate_presence_of(:name) }
+
+  it { is_expected.to validate_presence_of(:public_id) }
+
+  it { is_expected.to validate_uniqueness_of(:public_id) }
+
+  it { is_expected.to validate_uniqueness_of(:access_token) }
+
+  it { is_expected.to validate_length_of(:public_id).is_equal_to(10) }
+
+  it { is_expected.to validate_length_of(:access_token).is_equal_to(32) }
+
+  it 'does not change an existing public_id' do
+    access_token.save!
+    loaded_access_token = described_class.find(access_token.id)
+
+    expect(access_token.public_id).to eq(loaded_access_token.public_id)
+  end
+
+  it 'does not change an existing access_token' do
+    access_token.save!
+    loaded_access_token = described_class.find(access_token.id)
+
+    expect(access_token.access_token).to eq(loaded_access_token.access_token)
+  end
+end
