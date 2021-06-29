ba6e15b35a49062f9b961a7a4b2fa368197b11ac
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-29 13:02:40 -0700

    Fix videos controller api response (#13)
    * Add created_at to serializer response
    
    * Add created_at to api response
    
    * Order items bur creation date desc

--- app/serializers/access_token_serializer.rb
+++ app/serializers/access_token_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AccessTokenSerializer < ActiveModel::Serializer
-  attributes :id, :name, :access_token
+  attributes :id, :name, :access_token, :created_at
 
   def id
     object.public_id
--- app/serializers/signature_key_serializer.rb
+++ app/serializers/signature_key_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SignatureKeySerializer < ActiveModel::Serializer
-  attributes :id, :name, :signature_key
+  attributes :id, :name, :signature_key, :created_at
 
   def id
     object.public_id
