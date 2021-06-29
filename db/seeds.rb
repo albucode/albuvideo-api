8e14b41598d7615b86fbb460dfdb4823829fd38b
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-29 15:30:16 -0700

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create({ email: 'a@a.com', password: 'asdasdasd' })
+video = Video.create({ title: 'Video1', published: true, source: 'sourceseed', user_id: user.id })
+
+(1..3).map do
+  VideoWatchEvent.create({ duration: 2.5, video_id: video.id })
+end
