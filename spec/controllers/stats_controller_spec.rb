bf503d2449098464f6dcf4bf008dd3fcd171906f
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-30 14:47:36 -0700

    Test controller

---
+++ spec/controllers/stats_controller_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe StatsController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+
+  let(:video) { FactoryBot.create(:video) }
+
+  let(:video_watch_event) { FactoryBot.create(:video_watch_event, video_id: video.id, user_id: user.id) }
+  let(:video_watch_event2) { FactoryBot.create(:video_watch_event, video_id: video.id, user_id: user.id, duration: 2) }
+
+  describe 'get total watch time' do
+    before do
+      sign_in(user)
+    end
+
+    let(:request) do
+      get :total_watch_time, as: :json
+    end
+
+    it 'returns a the sum of all duration for user\'s video watch time' do
+      video_watch_event
+      video_watch_event2
+      body = JSON.parse(request.body)
+      expect(body).to match({ 'stats' => { 'time_watched' => 4 } })
+    end
+
+    it 'returns 0 when user has not created any video_watch_events' do
+      body = JSON.parse(request.body)
+      expect(body).to match({ 'stats' => { 'time_watched' => 0 } })
+    end
+  end
+end
