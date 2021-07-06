75ac0c62fce040afa76c524b2d22c6fd4a6bcd0b
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-06 12:30:36 -0700

    Create video Variant

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -5,6 +5,7 @@ class Variant < ApplicationRecord
 
   belongs_to :video
   has_many :segments, dependent: :destroy
+  has_one_attached :transcoded_file
 
   validates :height, :width, :bitrate, numericality: { greater_than: 0 }
 end
--- app/services/attach_source_file.rb
+++ app/services/attach_source_file.rb
@@ -9,5 +9,17 @@ class AttachSourceFile
     extension = File.extname(path)
     video.source_file.attach(io: tempfile, filename: "#{video.public_id}#{extension}")
     File.delete(path)
+
+    variant = Variant.create!(height: 1080, width: 1920, video_id: video.id, bitrate: 1)
+
+    video.source_file.open do |file|
+      input_file_path = file.path
+      output_file_path = "/tmp/#{video.public_id}.mp4"
+      system "ffmpeg -i #{input_file_path} -y -c:v libx264 -crf 22 -pix_fmt +yuv420p -vf scale=1920x1080 -b:a 192k #{output_file_path}"
+
+      transcoded_file = File.open(output_file_path)
+
+      variant.transcoded_file.attach(io: transcoded_file, filename: "#{video.public_id}.mp4")
+    end
   end
 end
