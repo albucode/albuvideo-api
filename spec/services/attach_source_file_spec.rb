79b2cac8c4bfb1e1e08bd53fe632b597229b05c1
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-18 21:22:44 -0700

    Stub request to external video

--- spec/services/attach_source_file_spec.rb
+++ spec/services/attach_source_file_spec.rb
@@ -5,6 +5,12 @@ require 'rails_helper'
 RSpec.describe AttachSourceFile do
   subject(:video) { FactoryBot.build(:video) }
 
+  before do
+    file = File.open(Rails.root.join('spec/support/minimal-video-with-audio.mp4'))
+    stub_request(:get, 'https://albuvideo.sfo3.digitaloceanspaces.com/dev/minimal-video-with-audio.mp4')
+      .to_return(status: 200, body: file, headers: {})
+  end
+
   it 'attaches a source_file to a video' do
     described_class.perform(video)
 
--- spec/services/transcode_source_file_spec.rb
+++ spec/services/transcode_source_file_spec.rb
@@ -5,6 +5,12 @@ require 'rails_helper'
 RSpec.describe TranscodeSourceFile do
   let(:video) { FactoryBot.create(:video) }
 
+  before do
+    file = File.open(Rails.root.join('spec/support/minimal-video-with-audio.mp4'))
+    stub_request(:get, 'https://albuvideo.sfo3.digitaloceanspaces.com/dev/minimal-video-with-audio.mp4')
+      .to_return(status: 200, body: file, headers: {})
+  end
+
   it 'attaches a transcoded_file to a variant' do
     AttachSourceFile.perform(video)
     described_class.perform(video, 1920, 1080)
