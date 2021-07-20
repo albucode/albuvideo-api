eb2238c506a70a0b6df37825689b8b5b66735e2c
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-20 15:04:03 -0700

    Return total time stored

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,6 +6,20 @@ class StatsController < ApplicationController
   def show
     total_stream_time = VideoStreamEvent.where(user: current_user).sum(:duration).round
 
-    render json: { stats: { time_streamed: total_stream_time } }
+    videos = Video.where(user: current_user, status: 'ready')
+    duration = videos.map { |video| video.source_file.blob.metadata[:duration].to_f }
+    total_time_stored = duration.sum.round
+    puts 'Aaaaaaaaaaaaaaaaa'
+    puts total_time_stored
+
+    render json: { stats: { time_streamed: total_stream_time, time_stored: total_time_stored } }
   end
 end
+
+
+
+
+#tentar substituir map por sum
+
+# total video stream
+# Video.last.source_file.blob.metadata[:duration]
