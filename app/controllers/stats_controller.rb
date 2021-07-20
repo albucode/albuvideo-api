7b0b5a93f76e556488cbcc2489024f7a2cdc65c1
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-20 15:08:01 -0700

    Replace map for sum

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,19 +7,9 @@ class StatsController < ApplicationController
     total_stream_time = VideoStreamEvent.where(user: current_user).sum(:duration).round
 
     videos = Video.where(user: current_user, status: 'ready')
-    duration = videos.map { |video| video.source_file.blob.metadata[:duration].to_f }
-    total_time_stored = duration.sum.round
-    puts 'Aaaaaaaaaaaaaaaaa'
-    puts total_time_stored
+    duration = videos.sum { |video| video.source_file.blob.metadata[:duration].to_f }
+    total_time_stored = duration.round
 
     render json: { stats: { time_streamed: total_stream_time, time_stored: total_time_stored } }
   end
 end
-
-
-
-
-#tentar substituir map por sum
-
-# total video stream
-# Video.last.source_file.blob.metadata[:duration]
