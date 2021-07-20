5659890be925e42584d5fca83afda69e6780f6db
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-20 16:01:10 -0700

    Return total time streamed in last 24h

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,10 +6,14 @@ class StatsController < ApplicationController
   def show
     total_stream_time = VideoStreamEvent.where(user: current_user).sum(:duration).round
 
+    stream_time_last_24h = VideoStreamEvent.where(user: current_user,
+                                                  created_at: 24.hours.ago..Time.zone.now).sum(:duration).round
+
     videos = Video.where(user: current_user, status: 'ready')
     duration = videos.sum { |video| video.source_file.blob.metadata[:duration].to_f }
     total_time_stored = duration.round
 
-    render json: { stats: { time_streamed: total_stream_time, time_stored: total_time_stored } }
+    render json: { stats: { time_streamed: total_stream_time, time_stored: total_time_stored,
+                            time_streamed_last_24h: stream_time_last_24h } }
   end
 end
