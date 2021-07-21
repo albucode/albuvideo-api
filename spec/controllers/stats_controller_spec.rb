f54f70dbb30a321cdf170ad51630feb285a461bd
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-21 12:08:30 -0700

    Refactor tests

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -7,21 +7,28 @@ RSpec.describe StatsController, type: :controller do
 
   let(:video) { FactoryBot.create(:video) }
 
-  let(:video_stream_event) { FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id) }
+  let(:video_stream_event) do
+    FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, created_at: 10.minutes.ago)
+  end
 
   let(:video_stream_event2) do
-    FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, duration: 2)
+    FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, duration: 2, created_at: 2.days.ago)
+  end
+
+  let(:video_stream_event3) do
+    FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, duration: 2,
+                                           created_at: 12.minutes.ago)
+  end
+
+  let(:request) do
+    get :show, as: :json
+  end
+
+  before do
+    sign_in(user)
   end
 
   describe 'get total stream time' do
-    before do
-      sign_in(user)
-    end
-
-    let(:request) do
-      get :show, as: :json
-    end
-
     it 'returns a the sum of all duration for user\'s video stream time' do
       video_stream_event
       video_stream_event2
@@ -36,27 +43,12 @@ RSpec.describe StatsController, type: :controller do
   end
 
   describe 'get total stream time for the last 24h' do
-    before do
-      sign_in(user)
-    end
-
-    let(:video_stream_event) do
-      FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, created_at: 10.minutes.ago)
-    end
-
-    let(:video_stream_event2) do
-      FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, duration: 2, created_at: 2.days.ago)
-    end
-
-    let(:request) do
-      get :show, as: :json
-    end
-
     it 'returns a the sum of all duration for user\'s video stream time for the last 24h' do
       video_stream_event
       video_stream_event2
+      video_stream_event3
       body = JSON.parse(request.body)
-      expect(body['stats']['time_streamed_last_24h']).to eq(2)
+      expect(body['stats']['time_streamed_last_24h']).to eq(4)
     end
 
     it 'returns 0 when user has not created any video_stream_events on the last 24h' do
