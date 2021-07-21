27df5ada7e26acf23e3eecf4679d66ce36549418
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-21 11:27:19 -0700

    Test time_streamed_last_24h response

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe StatsController, type: :controller do
   let(:video) { FactoryBot.create(:video) }
 
   let(:video_stream_event) { FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id) }
+
   let(:video_stream_event2) do
     FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, duration: 2)
   end
@@ -31,6 +32,37 @@ RSpec.describe StatsController, type: :controller do
     it 'returns 0 when user has not created any video_stream_events' do
       body = JSON.parse(request.body)
       expect(body['stats']['time_streamed']).to eq(0)
+    end
+  end
+
+  describe 'get total stream time for the last 24h' do
+    before do
+      sign_in(user)
+    end
+
+    let(:video_stream_event) do
+      FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, created_at: 10.minutes.ago)
+    end
+
+    let(:video_stream_event2) do
+      FactoryBot.create(:video_stream_event, video_id: video.id, user_id: user.id, duration: 2, created_at: 2.days.ago)
+    end
+
+    let(:request) do
+      get :show, as: :json
+    end
+
+    it 'returns a the sum of all duration for user\'s video stream time for the last 24h' do
+      video_stream_event
+      video_stream_event2
+      body = JSON.parse(request.body)
+      expect(body['stats']['time_streamed_last_24h']).to eq(2)
+    end
+
+    it 'returns 0 when user has not created any video_stream_events on the last 24h' do
+      video_stream_event2
+      body = JSON.parse(request.body)
+      expect(body['stats']['time_streamed_last_24h']).to eq(0)
     end
   end
 end
