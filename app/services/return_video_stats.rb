5722b6e6c71349195846e0b58ae2f6f72769cf56
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-22 10:29:05 -0700

    Extract stats into private methods

--- app/services/return_video_stats.rb
+++ app/services/return_video_stats.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
 class ReturnVideoStats
-  include AbstractController::Rendering
+  class << self
+    def perform(user)
+      { stats: { time_streamed: total_stream_time(user), time_stored: total_time_stored(user),
+                 time_streamed_last_24h: stream_time_last_24h(user) } }
+    end
 
-  def self.perform(user)
-    total_stream_time = VideoStreamEvent.where(user: user).sum(:duration).round
+    private
 
-    stream_time_last_24h = VideoStreamEvent.where(user: user,
-                                                  created_at: 24.hours.ago..Time.zone.now).sum(:duration).round
+    def total_stream_time(user)
+      VideoStreamEvent.where(user: user).sum(:duration).round
+    end
 
-    videos = Video.where(user: user, status: 'ready')
-    duration = videos.sum { |video| video.source_file.blob.metadata[:duration].to_f }
-    total_time_stored = duration.round
+    def stream_time_last_24h(user)
+      VideoStreamEvent.where(user: user, created_at: 24.hours.ago..Time.zone.now).sum(:duration).round
+    end
 
-    { stats: { time_streamed: total_stream_time, time_stored: total_time_stored,
-               time_streamed_last_24h: stream_time_last_24h } }
+    def total_time_stored(user)
+      videos = Video.where(user: user, status: 'ready')
+      duration = videos.sum { |video| video.source_file.blob.metadata[:duration].to_f }
+      duration.round
+    end
   end
 end
