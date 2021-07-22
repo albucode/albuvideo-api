3da7c99f7060e62ee9bb817e09b658176ecc0e60
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-22 10:53:00 -0700

    Ch95/create statistics on endpoints (#26)
    * Return total time stored
    
    * Replace map for sum
    
    * Return total time streamed in last 24h
    
    * Fix stats controller specs
    
    * Test time_streamed_last_24h response
    
    * Refactor tests
    
    * Extract method into ReturnVideoStats class
    
    * Extract stats into private methods
    
    * Rename file and class

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,8 +4,6 @@ class StatsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    total_stream_time = VideoStreamEvent.where(user: current_user).sum(:duration).round
-
-    render json: { stats: { time_streamed: total_stream_time } }
+    render json: UserStats.perform(current_user)
   end
 end
---
+++ app/services/user_stats.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class UserStats
+  class << self
+    def perform(user)
+      { stats: { time_streamed: total_stream_time(user), time_stored: total_time_stored(user),
+                 time_streamed_last_24h: stream_time_last_24h(user) } }
+    end
+
+    private
+
+    def total_stream_time(user)
+      VideoStreamEvent.where(user: user).sum(:duration).round
+    end
+
+    def stream_time_last_24h(user)
+      VideoStreamEvent.where(user: user, created_at: 24.hours.ago..Time.zone.now).sum(:duration).round
+    end
+
+    def total_time_stored(user)
+      videos = Video.where(user: user, status: 'ready')
+      duration = videos.sum { |video| video.source_file.blob.metadata[:duration].to_f }
+      duration.round
+    end
+  end
+end
