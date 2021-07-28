656f288076b6eea97190a4b9c63aa9207294d1bb
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-28 13:08:55 -0700

    Change stats into nested route

--- app/controllers/video_stats_controller.rb
+++ app/controllers/video_stats_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class VideoStatsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     query = <<~SQL.squish
       SELECT sum(duration), time_bucket_gapfill('1 hour', video_stream_events.created_at) AS period
       FROM video_stream_events
       LEFT JOIN videos v on video_stream_events.video_id = v.id
-      WHERE(public_id = '#{params[:id]}' AND video_stream_events.created_at BETWEEN NOW() - interval '24 hours' AND NOW())
+      WHERE(public_id = '#{params[:video_id]}' AND video_stream_events.created_at BETWEEN NOW() - interval '48 hours' AND NOW())
       GROUP BY period;
     SQL
     render json: ActiveRecord::Base.connection.execute(query).to_a
