1ad6925997a34de209a652d53ca29c47c829a8e8
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-29 11:38:52 -0700

    Extract SQL query to model method

--- app/controllers/video_stats_controller.rb
+++ app/controllers/video_stats_controller.rb
@@ -4,13 +4,8 @@ class VideoStatsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    query = <<~SQL.squish
-      SELECT sum(duration), time_bucket_gapfill('1 hour', video_stream_events.created_at) AS period
-      FROM video_stream_events
-      LEFT JOIN videos v on video_stream_events.video_id = v.id
-      WHERE(public_id = '#{params[:video_id]}' AND video_stream_events.created_at BETWEEN NOW() - interval '48 hours' AND NOW())
-      GROUP BY period;
-    SQL
+    video = Video.find_by(public_id: params[:video_id], user: current_user)
+    query = video.hourly_stream_time_last_24h
     render json: ActiveRecord::Base.connection.execute(query).to_a
   end
 end
