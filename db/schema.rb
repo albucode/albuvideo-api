3fb3d32afd0c95e35bc4e2d13186f53119d242cc
Gui Vieira <guialbuk@gmail.com>
2021-08-03 22:42:46 -0700

    Add session_id column to video_stream_events table (#35)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_002837) do
+ActiveRecord::Schema.define(version: 2021_08_04_052315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_002837) do
     t.float "duration", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "session_id"
     t.index ["user_id"], name: "index_video_stream_events_on_user_id"
     t.index ["video_id"], name: "index_video_stream_events_on_video_id"
   end
