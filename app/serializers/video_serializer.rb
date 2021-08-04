3259dc8da042894f1a3f19f8839c6796e8d1935e
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-04 12:37:55 -0700

    Serialize video times watched

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -2,7 +2,7 @@
 
 class VideoSerializer < ActiveModel::Serializer
   attributes :id, :title, :status, :published, :source, :created_at, :total_stream_time, :stream_time_last_24h,
-             :playlist_url
+             :playlist_url, :times_watched
 
   def playlist_url
     "http://localhost:8000/videos/#{id}.m3u8"
