969b85f22ede41864b05a546c2e3dd13fa0fa5b7
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-04 10:24:10 -0700

    Ch76/display streaming links on video show page (#34)
    * Create link to video playlist
    
    * Test if playlist_key contains the correct value
    
    * Serialize playlist_url

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :title, :status, :published, :source, :created_at, :total_stream_time, :stream_time_last_24h
+  attributes :id, :title, :status, :published, :source, :created_at, :total_stream_time, :stream_time_last_24h,
+             :playlist_url
+
+  def playlist_url
+    "http://localhost:8000/videos/#{id}.m3u8"
+  end
 
   def id
     object.public_id
