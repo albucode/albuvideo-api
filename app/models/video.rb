d2dc06d2834a93457d0b712415dd5d766078508a
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-09 11:04:58 -0700

    Change chart interval value

--- app/models/video.rb
+++ app/models/video.rb
@@ -46,7 +46,7 @@ class Video < ApplicationRecord
       SELECT sum(duration), time_bucket_gapfill('1 hour', video_stream_events.created_at) AS period
       FROM video_stream_events
       LEFT JOIN videos v on video_stream_events.video_id = v.id
-      WHERE(public_id = '#{public_id}' AND video_stream_events.created_at BETWEEN NOW() - interval '48 hours' AND NOW())
+      WHERE(public_id = '#{public_id}' AND video_stream_events.created_at BETWEEN NOW() - interval '24 hours' AND NOW())
       GROUP BY period;
     SQL
     ActiveRecord::Base.connection.execute(query).to_a
