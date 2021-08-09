169bf2b6cad779aead83aadc6fb192bbeb146766
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-09 12:00:35 -0700

    Pass interval and frequency as params to frequency interval

--- app/models/video.rb
+++ app/models/video.rb
@@ -41,12 +41,12 @@ class Video < ApplicationRecord
     VideoStreamEvent.where(video: id).select(:session_id).distinct.count
   end
 
-  def hourly_stream_time_last_24h
+  def stream_time_data(frequency, interval)
     query = <<~SQL.squish
-      SELECT sum(duration), time_bucket_gapfill('1 hour', video_stream_events.created_at) AS period
+      SELECT sum(duration), time_bucket_gapfill( '#{frequency}', video_stream_events.created_at) AS period
       FROM video_stream_events
       LEFT JOIN videos v on video_stream_events.video_id = v.id
-      WHERE(public_id = '#{public_id}' AND video_stream_events.created_at BETWEEN NOW() - interval '24 hours' AND NOW())
+      WHERE(public_id = '#{public_id}' AND video_stream_events.created_at BETWEEN NOW() - interval '#{interval}' AND NOW())
       GROUP BY period;
     SQL
     ActiveRecord::Base.connection.execute(query).to_a
