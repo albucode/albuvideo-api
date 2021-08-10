e0123dcd9f116f87657cf87aa33e4ded0b4f1500
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-09 18:32:30 -0700

    Ch110/display chart options (#38)
    * Change chart interval value
    
    * Pass interval and frequency as params to frequency interval
    
    * Fix video model spec
    
    * Fix video controller specs
    
    * Sanitize SQL query

--- app/controllers/video_stats_controller.rb
+++ app/controllers/video_stats_controller.rb
@@ -5,6 +5,6 @@ class VideoStatsController < ApplicationController
 
   def show
     video = Video.find_by!(public_id: params[:video_id], user: current_user)
-    render json: video.hourly_stream_time_last_24h
+    render json: video.stream_time_data(params[:frequency], params[:interval])
   end
 end
