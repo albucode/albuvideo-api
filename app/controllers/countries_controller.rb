318dff796e237c016af80ba4a7d6b503e807a32d
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-25 10:39:34 -0700

    Authenticate endpoint

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CountriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     countries = Country.all
     render json: countries
