02614ffdad6b6d3e60768019735fdd5d8f664d75
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-25 09:41:27 -0700

    Add validation to country

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,4 +3,7 @@
 class Country < ApplicationRecord
   has_many :country_permissions, dependent: :destroy
   has_many :videos, through: :country_permissions
+
+  validates :name, :code, presence: true
+  validates :code, uniqueness: true
 end
