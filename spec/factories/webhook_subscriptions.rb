5206f2c241393a8a447168970109c791bc8984e2
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-31 12:51:57 -0700

    Validate https for webhook url

--- spec/factories/webhook_subscriptions.rb
+++ spec/factories/webhook_subscriptions.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :webhook_subscription do
     topic { 'video/ready' }
-    url { Faker::Internet.url }
+    url { Faker::Internet.url(scheme: 'https') }
     user
   end
 end
