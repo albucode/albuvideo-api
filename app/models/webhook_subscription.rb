dbd747921492dd927b310e7d8a80a6004c0b5d3f
Aline Ribeiro <alinear.mail@gmail.com>
2021-09-01 10:01:46 -0700

    Validate https for webhook url (#46)

--- app/models/webhook_subscription.rb
+++ app/models/webhook_subscription.rb
@@ -6,7 +6,7 @@ class WebhookSubscription < ApplicationRecord
   belongs_to :user
 
   validates :topic, :url, presence: true
-  validates :url, url: true
+  validates :url, url: { schemes: ['https'] }
   validates :topic, inclusion: { in: %w[video/ready video/failed] }
   validates :url, uniqueness: { scope: %w[topic user_id], case_sensitive: false }
 end
