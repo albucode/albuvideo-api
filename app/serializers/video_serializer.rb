7c790c6992da584654fafdcd329007297deaefe8
Aline Ribeiro <alinear.mail@gmail.com>
2021-09-08 11:36:18 -0700

    Serialize country list and permission

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -2,7 +2,7 @@
 
 class VideoSerializer < ActiveModel::Serializer
   attributes :id, :title, :status, :published, :source, :created_at, :total_stream_time, :stream_time_last_24h,
-             :playlist_url, :times_watched
+             :playlist_url, :times_watched, :country_permission_type, :countries
 
   def playlist_url
     "http://localhost:8000/videos/#{id}.m3u8"
@@ -11,4 +11,6 @@ class VideoSerializer < ActiveModel::Serializer
   def id
     object.public_id
   end
+
+  delegate :countries, to: :object
 end
