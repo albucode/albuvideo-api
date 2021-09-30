3f15653f654a0c59542f2bec38fd0367a8f5f633
Aline Ribeiro <alinear.mail@gmail.com>
2021-09-29 17:24:51 -0700

    Calculate previous_month_streaming_time

--- app/jobs/create_invoice_job.rb
+++ app/jobs/create_invoice_job.rb
@@ -10,8 +10,14 @@ class CreateInvoiceJob < ApplicationJob
     User.all.each do |user|
       invoice = Invoice.create!(start_date: beginning_of_last_month, end_date: end_of_last_month, user_id: user.id,
                                 status: 0)
-      InvoiceItem.create!(service_id: service.id, quantity: 1.0, invoice_id: invoice.id, user_id: user.id,
+      InvoiceItem.create!(service_id: service.id, quantity: previous_month_streaming_time(user.id), invoice_id: invoice.id, user_id: user.id,
                           price: service.price)
     end
+  end
+
+  def previous_month_streaming_time(id)
+    beginning_of_last_month = Time.zone.now.last_month.beginning_of_month
+    end_of_last_month = Time.zone.now.last_month.end_of_month
+    VideoStreamEvent.where(user_id: id, created_at: beginning_of_last_month..end_of_last_month).sum(:duration)
   end
 end
