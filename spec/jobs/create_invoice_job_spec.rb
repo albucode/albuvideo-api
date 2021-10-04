10c8d32e00ec1b564af00cd57c716c6ebf79ac3f
Aline Ribeiro <alinear.mail@gmail.com>
2021-10-04 09:51:46 -0700

    Test creation of Invoice and InvoiceItem

--- spec/jobs/create_invoice_job_spec.rb
+++ spec/jobs/create_invoice_job_spec.rb
@@ -3,4 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe CreateInvoiceJob, type: :job do
+  let(:service) { FactoryBot.create(:service) }
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    user
+    service
+  end
+
+  it 'creates a invoice' do
+    expect do
+      described_class.new.perform_now
+    end.to change(Invoice, :count).by(1)
+  end
+
+  it 'creates a invoice_item' do
+    expect do
+      described_class.new.perform_now
+    end.to change(InvoiceItem, :count).by(1)
+  end
 end
