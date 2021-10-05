4c5e3dcde23435f0f442905e843273faf881fa77
Aline Ribeiro <alinear.mail@gmail.com>
2021-10-05 15:08:19 -0700

    Serialize item_total

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class InvoiceItemSerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :user_id, :service_id, :price, :invoice_id
+  attributes :id, :quantity, :user_id, :service_id, :price, :invoice_id, :item_total
+
+  def item_total
+    object.quantity * object.price
+  end
 end
