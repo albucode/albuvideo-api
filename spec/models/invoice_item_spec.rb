91657ce0e8702d188fcda2f2774d9ecd301dade6
Aline Ribeiro <alinear.mail@gmail.com>
2021-10-05 16:37:59 -0700

    Test total methods

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
+  let(:invoice_item) { FactoryBot.create(:invoice_item, quantity: 10, price: 20) }
+
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
 
@@ -21,5 +23,9 @@ RSpec.describe InvoiceItem, type: :model do
     it { validate_numericality_of(:price).is_greater_than(0) }
 
     it { validate_numericality_of(:price).is_less_than(1_000) }
+  end
+
+  it 'calculates invoice_item total' do
+    expect(invoice_item.item_total).to eq(200)
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,9 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:invoice) { FactoryBot.create(:invoice, user_id: user.id) }
+  let(:invoice_item) do
+    FactoryBot.create(:invoice_item, quantity: 10, price: 20, invoice_id: invoice.id, user_id: user.id)
+  end
+  let(:invoice_item2) do
+    FactoryBot.create(:invoice_item, quantity: 10, price: 30, invoice_id: invoice.id, user_id: user.id)
+  end
+
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
 
     it { is_expected.to have_many(:invoice_items) }
+  end
+
+  it 'calculates invoice_total' do
+    invoice_item
+    invoice_item2
+    expect(invoice.invoice_total).to eq(500)
   end
 end
